5cd2f850a722b7c2a4361a2057e29c597b27733c
Ezekiel Adetoro <39957559+adetorodev@users.noreply.github.com>
2024-08-27 23:35:08 +0100

    add to bloc

--- todo_list/lib/cubit/todo_cubit.dart
+++ todo_list/lib/cubit/todo_cubit.dart
@@ -3,4 +3,11 @@ import 'package:todo_list/models/todo_models.dart';
 
 class TodoCubit extends Cubit<List<Todos>> {
   TodoCubit() : super([]);
+
+  void addTodo(String title) {
+    final todo = (name: title, createdAt: DateTime.now());
+
+    state.add(todo as Todos);
+    emit(state);
+  }
 }
