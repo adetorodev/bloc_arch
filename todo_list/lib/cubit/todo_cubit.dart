cdcf913e80e0a01e9f56d8d7374af4db8576ee65
Ezekiel Adetoro <39957559+adetorodev@users.noreply.github.com>
2024-09-04 17:49:00 +0100

    Todo List App

--- todo_list/lib/cubit/todo_cubit.dart
+++ todo_list/lib/cubit/todo_cubit.dart
@@ -5,9 +5,25 @@ class TodoCubit extends Cubit<List<Todo>> {
   TodoCubit() : super([]);
 
   void addTodo(String title) {
+    if (title.isEmpty) {
+      addError("Title cannot be empty");
+      return;
+    }
     final todo = Todo(name: title, createdAt: DateTime.now());
 
     // state.add(todo as Todos);
     emit([...state, todo]);
+  }
+
+  @override
+  void onChange(Change<List<Todo>> change) {
+    super.onChange(change);
+    print('TodoCubit - $change');
+  }
+
+  @override
+  void onError(Object error, StackTrace stackTrace) {
+    super.onError(error, stackTrace);
+    print(error);
   }
 }
